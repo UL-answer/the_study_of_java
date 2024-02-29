dad4ad3ba8e063e7bdf97876109e1a0589ee7458
Apig <84331758+UL-answer@users.noreply.github.com>
2024-03-01 00:40:30 +0800

    Delete Thread_process directory

--- Thread_process/project2/ThreadPriority.java
+++
@@ -1,9 +0,0 @@
-package Thread_process.project2;
-
-public class ThreadPriority extends Thread{
-    public void run(){
-        for (int i=0;i<100;i++){
-            System.out.println(getName()+":"+i);
-        }
-    }
-}
